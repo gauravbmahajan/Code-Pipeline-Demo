2dd4b2d3396beee228c88c5dad931419239c15d1
Gaurav Mahajan <gauravbmahajan@gmail.com>
2023-02-20 19:49:11 -0700

    initial commit

---
+++ src/test/java/com/gm/codepipeline/CodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.codepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
